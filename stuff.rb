20752280386145f606a45976ea7d2be0aab467ed
Paulo Schneider <paulo.schneider@gmail.com>
2009-05-21 22:47:00 +0100

    Playing around with the flickr API

--- stuff.rb
+++ stuff.rb
@@ -1,7 +1,16 @@
 require "rubygems"
 require "flickr"
-require "net/http"
+require "yaml"
 
-flickr = Flickr.new("blah")
-puts Net::HTTP.get_response(URI.parse(flickr.request_url("blah")))
-#puts flickr.users("paulo.schneider@gmail.com").name
+def get(flickr, letter)
+  photo = flickr.photos(:tags => "oneletter, #{letter}", :tag_mode => "all", :per_page => "1").first
+  # File.open(photo.filename, 'w') { |file| file.puts photo.file('Square') }
+end
+
+config = YAML.load_file("flickr_key.yaml")
+
+flickr = Flickr.new(config["key"])
+get(flickr, "b")
+get(flickr, "l")
+get(flickr, "a")
+get(flickr, "h")
