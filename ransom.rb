fd7fbe6707ffefc70d67dd6b3d5cde3c37a0f6b8
Paulo Schneider <paulo.schneider@gmail.com>
2009-06-02 23:32:29 +0100

    Adding digits and more randomization

--- ransom.rb
+++ ransom.rb
@@ -5,8 +5,7 @@ require "haml"
 require "sass"
 
 get '/' do
-  # message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  message = "Ransom is the practice of holding a prisoner to extort money or property to secure their release, or it can refer to the sum of money involved."
   @photo_urls = RansomFlickr.new("flickr_key.yaml").get_photo_urls(message)
   haml :view
 end
--- ransom_flickr.rb
+++ ransom_flickr.rb
@@ -6,20 +6,46 @@ class RansomFlickr
  
   def initialize(config_file)
     @flickr = Flickr.new(YAML.load_file(config_file)["key"])
-  end
- 
-  def get(letter)
-    @flickr.photos(:tags => "oneletter, #{letter}", :tag_mode => "all", :per_page => "1").first
+    @url_cache = Hash.new
+    @punctuation_map = {
+      "!" => "exclamation",
+      "." => "period",
+      "," => "comma",
+      ";" => "semicolon",
+      "?" => "question mark",
+      "-" => "dash",
+      ":" => "colon"
+    }
   end
   
   def get_photo_urls(message)
-    message.split("").map do |letter|
-      if letter.match(/[A-Za-z]/)
-        get(letter).source("Square")
+    message.split(/\s/).map do |word|
+      word.split("").inject([]) do |photo_word, letter|
+        photo_word << get_url_for(letter)
+      end.compact
+    end
+  end
+  
+  private
+  
+  def get_url_for(char)
+    unless @url_cache.has_key?(char)
+      if char.match(/[A-Za-z]/)
+        @url_cache[char] = fetch(char, "oneletter")
+      elsif char.match(/[0-9]/)
+        @url_cache[char] = fetch(char, "onedigit")
+      elsif @punctuation_map.has_key?(char)
+        @url_cache[char] = fetch(@punctuation_map[char], "punctuation")
       else
-        nil
+        @url_cache[char] = [nil]
       end
     end
+    urls = @url_cache[char]
+    return urls[rand(urls.size)]
+  end
+  
+  def fetch(char, group)
+    @flickr.photos(:tags => "#{group}, #{char}", :tag_mode => "all", :per_page => "10").map { |photo| photo.source("Square") }
   end
   
 end
