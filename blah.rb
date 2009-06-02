0d3fbd0b4975167ed1be937e4437964799359fdd
moonpxi <paulo.schneider@gmail.com>
2009-06-02 16:04:04 +0100

    First sinatra version

--- blah.rb
+++ blah.rb
@@ -1,5 +1,21 @@
 require 'rubygems'
+require "ransom_flickr"
+
 require 'sinatra'
-get '/hi' do
-  "Hello World!"
+get '/' do
+  flickr = RansomFlickr.new("flickr_key.yaml")  
+  return getimg("i blame jason now") 
+end
+
+def getimg(message)
+  flickr = RansomFlickr.new("flickr_key.yaml")
+  html = ""
+  message.each_char do |letter|
+    if letter != " "
+      html += "<img src=\"#{flickr.get(flickr, letter).source('Square')}\" />"
+    else
+      html += "<br/>"
+    end
+  end
+  return html
 end
--- stuff.rb
+++ stuff.rb
@@ -1,7 +1,7 @@
-require "lib/ransom_flickr"
+require "ransom_flickr"
 
 flickr = RansomFlickr.new("flickr_key.yaml")
-flickr.get(flickr, "b")
-flickr.get(flickr, "l")
+puts flickr.get(flickr, "b").source('Square')
+puts flickr.get(flickr, "l").source('Square')
 flickr.get(flickr, "a")
 flickr.get(flickr, "h")
