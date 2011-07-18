4139603404862a2eca3b23da0c01fc235e863f63
moonpxi <paulo.schneider@gmail.com>
2011-07-18 21:09:51 +0100

    Filling in a bit of the content of the homepage

--- app/ransom.rb
+++ app/ransom.rb
@@ -1,6 +1,12 @@
 require "rubygems"
 require "sinatra"
+require "haml"
 
 get "/" do
  haml :homepage
+end
+
+get "/ransom.css" do
+   content_type "text/css", :charset => "utf-8"
+   sass :ransom
 end
