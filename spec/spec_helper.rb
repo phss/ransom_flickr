5153d9c2de01d76accb8aa6d04e015bed95aece5
moonpxi <paulo.schneider@gmail.com>
2011-09-16 11:41:30 +0100

    Including model on spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "rspec"
 require "rack/test"
 require File.dirname(__FILE__) + "/helpers/fake_image_service"
 require File.dirname(__FILE__) + "/../lib/flickr"
+require File.dirname(__FILE__) + "/../app/model"
 
 ENV['RACK_ENV'] = 'test'
 
