df2016fc407c457c224d6521001a386bde422e0a
moonpxi <paulo.schneider@gmail.com>
2011-09-16 12:15:06 +0100

    Composer ignores upper case of notes

--- app/model/composer.rb
+++ app/model/composer.rb
@@ -5,7 +5,7 @@ class Composer
   end
 
   def generate(note)
-    note.split("").inject([]) do |word, character|
+    note.downcase.split("").inject([]) do |word, character|
       word << @images.find_for(character).first
     end
   end
--- spec/app/model/composer_spec.rb
+++ spec/app/model/composer_spec.rb
@@ -8,17 +8,23 @@ describe Composer do
   end
 
   it "should generate note with first image of each character" do
-    images_should_receive_queries_for "a" => [Image.new("a1", "a1"), Image.new("a2", "a2")],
-                                      "b" => [Image.new("b1", "b1"), Image.new("b2", "b2")],
-                                      "c" => [Image.new("c1", "c1"), Image.new("c2", "c2")]
+    images_should_have "a" => [Image.new("a1", "a1"), Image.new("a2", "a2")],
+                       "b" => [Image.new("b1", "b1"), Image.new("b2", "b2")],
+                       "c" => [Image.new("c1", "c1"), Image.new("c2", "c2")]
 
     @composer.generate("abc") == [Image.new("a1", "a1"), Image.new("b1", "b1"), Image.new("c1", "c1")]
   end
 
+  it "should generate note ignoring case" do
+    images_should_have "a" => [Image.new("a1", "a1")]
 
-  def images_should_receive_queries_for(expected_image_map)
+    @composer.generate("aA") == [Image.new("a1", "a1"), Image.new("a1", "a1")]
+  end
+
+
+  def images_should_have(expected_image_map)
     expected_image_map.each do |character, expected_results|
-      @images.should_receive(:find_for).with(character).and_return(expected_results)
+      @images.stub(:find_for).with(character).and_return(expected_results)
     end
   end
 
